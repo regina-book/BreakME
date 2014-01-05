fa636891277d73d94499c53aa66a15e71c65c993
AiraneToufik <tf.airane@gmail.com>
2014-01-05 03:28:27 +0100

    CrackMe 01

--- CrackMex01.cpp
+++ CrackMex01.cpp
@@ -4,10 +4,11 @@
     //@CrackMex01
     //@Toufik Airane
     //@2013.10
-    //@Greetz 0ver
     /**/
-    #include<iostream>#include<ctime>using namespace std;
+    #include<iostream>
+    #include<ctime>
     #define NOTICE "We have to found the right number between 1 and 10000\n"\
                     "You got only ONE chance :)\n"
+    using namespace std;
     int main(){cout<<NOTICE;int pass,salt=time(NULL)-1382912020;salt=salt*salt%10000;
     cin>>pass;(pass==salt)?cout<<"GOOD JOB :) !!!\n":cout<<"BAD BOY :( !!!\n";return 0;}
