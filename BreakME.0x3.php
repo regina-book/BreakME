84958a30a7443c1ca5ecf7ebfb29f417ccfd5412
AiraneToufik <tf.airane@gmail.com>
2014-04-14 18:40:59 +0200

    BreakMe Unserialize

---
+++ BreakME.0x3.php
@@ -0,0 +1,15 @@
+<?php
+if(!isset($_GET['q'])) {
+setcookie("data",base64_encode(serialize(array(
+"secret" => "bdf2284c7b25e754cab8828c684c2ef845d1559530674674e73bbc6f38fa54a2"))));
+// vraiment très, très, très long
+}
+else
+{
+$data = unserialize(base64_decode($_COOKIE['data']));
+if($data['secret'] == hash("sha256",$_GET['q']))
+	echo "Facile :)";
+else
+	echo "Aie aie aie :(";
+}
+?>
