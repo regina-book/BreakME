4737759548618a6a447b1a15bc0b11ce8f0f133e
AiraneToufik <tf.airane@gmail.com>
2014-05-12 03:25:59 +0200

    add Paypal vuln

--- BreakME.0x3.php
+++ BreakME.0x3.php
@@ -2,14 +2,13 @@
 if(!isset($_GET['q'])) {
 setcookie("data",base64_encode(serialize(array(
 "secret" => "bdf2284c7b25e754cab8828c684c2ef845d1559530674674e73bbc6f38fa54a2"))));
-// vraiment très, très, très long
+// very very very long >o<
 }
 else
 {
 $data = unserialize(base64_decode($_COOKIE['data']));
-if($data['secret'] == hash("sha256",$_GET['q']))
-	echo "Facile :)";
-else
-	echo "Aie aie aie :(";
+if($data['secret'] != hash("sha256",$_GET['q']))
+    header('Location: http://tfairane.github.com');
+echo "YOU CANT SEE ME ! :D";
 }
 ?>
