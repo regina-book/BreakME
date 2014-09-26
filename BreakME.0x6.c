2727339ef59eea95aecf71825eaeea17393209f0
AiraneToufik <tf.airane@gmail.com>
2014-09-26 22:24:41 +0200

    BreakMe0x6
    An error slipped into this source code

---
+++ BreakME.0x6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * An error has slipped into this source code
+ * Change ONE letter to compile
+ */
+#define woman(t,h,i,n,k) h##i##n##t
+#define man woman(g,m,a,i,l)//tfairane[at]gmail[dot]com
+void man() {
+    printf(":%s\n", (0)?"(":")");
+}
